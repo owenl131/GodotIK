146fb69da11e9832ac51842be4cef765a0de9d8a
Owen Leong <owenl131@gmail.com>
2020-07-16 00:10:26 +0800

    First commit

---
+++ Bee.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public class Bee : Spatial
+{
+    public Vector3 target;
+    public Timer timer;
+    public MeshInstance mesh;
+    public Vector3 position;
+
+    public Bee()
+    {
+        target = GenerateRandomPosition();
+    }
+
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        position = Vector3.Zero;
+        SphereMesh sphere = new SphereMesh();
+        sphere.Radius = 0.3f;
+        sphere.Height = 0.6f;
+        mesh = new MeshInstance();
+        mesh.Mesh = sphere;
+        mesh.MaterialOverride = new SpatialMaterial()
+        {
+            AlbedoColor = Colors.Red
+        };
+        AddChild(mesh);
+
+        timer = new Timer();
+        AddChild(timer);
+        timer.Connect("timeout", this, "OnTimeout");
+        timer.Start(4);
+    }
+
+    public void OnTimeout()
+    {
+        target = GenerateRandomPosition();
+        GD.Print(target);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        float speed = 3f;
+        Vector3 dir = target - position;
+        if (dir.LengthSquared() > 1)
+            dir = dir.Normalized();
+        position += dir * speed * delta;
+        Transform newTransform = new Transform(Basis.Identity, position);
+        GlobalTransform = newTransform;
+        GD.Print(GlobalTransform.origin, target);
+    }
+    
+    public Vector3 GenerateRandomPosition()
+    {
+        float range = 3;
+        return new Vector3(
+            2 * range * GD.Randf() - range,
+            2 * range * GD.Randf() - range,
+            2 * range * GD.Randf() - range);
+    }
+
+}
---
+++ World.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class World : Spatial
+{
+    public Solver solver;
+    public Joint endpoint;
+    public Bee bee;
+
+    public override void _Ready()
+    {
+        Arm arm1 = new Arm(null, 0);
+        AddChild(arm1);
+
+        List<Joint> joints = new List<Joint>();
+        
+        Arm arm = arm1;
+        joints.Add(new Joint(arm, Vector3.Right));
+        arm = new Arm(joints[joints.Count - 1], 0);
+        joints.Add(new Joint(arm, Vector3.Up));
+        arm = new Arm(joints[joints.Count - 1]);
+        joints.Add(new Joint(arm, Vector3.Forward));
+        arm = new Arm(joints[joints.Count - 1]);
+        joints.Add(new Joint(arm, Vector3.Right));
+        arm = new Arm(joints[joints.Count - 1]);
+        endpoint = new Joint(arm, Vector3.Up);
+        endpoint.mesh.MaterialOverride = new SpatialMaterial() 
+        {
+            AlbedoColor = Colors.Blue
+        };
+
+        solver = new Solver()
+        {
+            endpoint = endpoint,
+            joints = joints
+        };
+
+        // solver.TestSVD(new float[,]{{1, 0}, {2, 0}});
+        // solver.TestSVD(new float[,]{{1, 2}, {3, 4}});
+        // solver.TestSVD(new float[,]{{1, 1}, {1, 1}});
+        // solver.TestSVD(new float[,]{{1, 3, 1}, {2, 4, 2}, {1, 3, 1}});
+        // solver.TestPseudoinverse(new float[,]{{1, 0}, {2, 0}});
+        // solver.TestPseudoinverse(new float[,]{{1, 2}, {3, 4}});
+        // solver.TestPseudoinverse(new float[,]{{1, 1}, {1, 1}});
+
+        bee = new Bee();
+        AddChild(bee);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        float scale = 2f;
+        float[] drotation = solver.ComputeIK(bee.GlobalTransform.origin, 0.2f);
+        for (int i = 0; i < drotation.Length; i++)
+        {
+            Joint joint = solver.joints[i];
+            joint.UpdateParameter(delta * drotation[i] * scale);
+        }
+    }
+
+}
